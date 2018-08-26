d83a2d82a7bc76b061d8994b51bd78cc421121e5
Oreder <stepup.ored@gmail.com>
2018-08-26 14:16:08 +0300

    Understanding of function literal

---
+++ 03 Functions/program.js
@@ -0,0 +1,9 @@
+var app = {};
+
+// Create a variable called add and store a function
+// in it that adds two numbers.
+app.add = function (a, b) {
+	return a + b;
+};
+
+document.writeln(app.add(3, 4));
