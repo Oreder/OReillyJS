da6ee74ce2e426fe50e55e65e591166e2b647c54
Oreder <stepup.ored@gmail.com>
2018-08-26 13:47:50 +0300

    Understanding of global abatement

---
+++ 02 Global abatement/program.js
@@ -0,0 +1,21 @@
+var app = {};
+
+app.stooge = {
+	"first-name": "Joe",
+	"last-name": "Howard"
+};
+
+app.flight = {
+	airline: "Oceanic",
+	number: 815,
+	departure: {
+		IATA: "SYD",
+		time: "2018-08-26 14:55",
+		city: "Sydney"
+	},
+	arrival: {
+		IATA: "LAX",
+		time: "2018-08-27 10:42",
+		city: "Los Angeles"
+	}
+};
