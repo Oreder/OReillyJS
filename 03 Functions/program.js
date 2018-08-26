db823d4bd5b8747826e27b73637048671e8e31ad
Oreder <stepup.ored@gmail.com>
2018-08-26 14:21:45 +0300

    Understanding of the method invocation pattern

--- 03 Functions/program.js
+++ 03 Functions/program.js
@@ -6,4 +6,15 @@ app.add = function (a, b) {
 	return a + b;
 };
 
-document.writeln(app.add(3, 4));
+app.myObject = {
+	value: 0,
+	increment: function(inc){
+		this.value += (typeof inc === 'number')? inc : 1;
+	}
+}
+
+app.myObject.increment();
+document.writeln(app.myObject.value);		// 1
+
+app.myObject.increment(-3);
+document.writeln(app.myObject.value);		// -2
